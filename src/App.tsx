8970e35966f67189c37a171a05ecc504e272c179
RiwajMaharjan <np03cs4a240114@heraldcollege.edu.np>
2026-03-29 08:24:51 +0545

    initial commit setting up the project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <div>Initial</div>
+  )
+}
+
+export default App
---
+++ src/constants/routes.ts
@@ -0,0 +1,20 @@
+export const ROUTES = {
+  HOME: '/',
+  LOGIN: '/login',
+  REGISTER: '/register',
+
+  // Patient
+  PATIENT_DASHBOARD: '/dashboard',
+  BOOK_APPOINTMENT: '/appointments/book',
+  MY_APPOINTMENTS: '/appointments',
+
+  // Doctor
+  DOCTOR_DASHBOARD: '/doctor/dashboard',
+  DOCTOR_SCHEDULE: '/doctor/schedule',
+  DOCTOR_PATIENTS: '/doctor/patients',
+
+  // Admin
+  ADMIN_DASHBOARD: '/admin',
+  MANAGE_DOCTORS: '/admin/doctors',
+  MANAGE_PATIENTS: '/admin/patients',
+} as const;
