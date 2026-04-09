aae52aee759402c5c3fa3eb12f05aab4cc13d7f0
RiwajMaharjan <np03cs4a240114@heraldcollege.edu.np>
2026-04-09 17:30:55 +0545

    fixed router and upcomingDoctor error

--- src/components/PatientDashboard/TopDoctors.tsx
+++ src/components/PatientDashboard/TopDoctors.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router";
-import { doctors } from "../../../data/clinicData";
+import { doctors } from "@/utils/clinicData";
 import { Star } from "lucide-react";
 
 const TopDoctors = () => {
